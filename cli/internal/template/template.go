78745f30e19fbb5f1ed9a2b9638d526522fe20d5
Alexander Bartolomey <occloxium@gmail.com>
2022-06-16 23:09:52 +0200

    feat(cli): rework template module to support custom templates as parameter
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/internal/template/template.go
+++ cli/internal/template/template.go
@@ -7,11 +7,34 @@ import (
 	_ "embed"
 
 	"github.com/Masterminds/sprig/v3"
+	"github.com/lithammer/dedent"
 	config "github.com/zoomoid/assignments/v1/internal/config"
 )
 
-//go:embed sheet.tmpl
-var sheetTemplate string
+var (
+	defaultSheetTemplate = dedent.Dedent(`
+		\documentclass{csassignments}
+		{{- range $_, $input := .Includes -}}
+		\input{ {{- $input -}} }
+		{{ end }}
+		\course{ {{- .Course -}} }
+		\group{ {{- .Group | default "" -}} }
+		\sheet{ {{- .Sheet | default "" -}} }
+		\due{ {{- .Due | default "" -}} }
+		{{- range $_, $member := .Members }}
+		{{- $firstname := ($member.Name | splitList " " | initial | join " ") | default "" -}}
+		{{- $lastname := ($member.Name | splitList " " | last) | default "" -}} 
+		\member{ {{- $firstname -}} }{ {{- $lastname -}} }{ {{- $member.ID -}} }
+		{{ end }}
+		\begin{document}
+		\maketitle
+		\gradingtable
+		
+		% Start the assignment here
+		
+		\end{document}
+	`)
+)
 
 type TemplateBinding struct {
 	ClassPath string
@@ -20,10 +43,14 @@ type TemplateBinding struct {
 	Sheet     string
 	Due       string
 	Members   []config.GroupMember
+	Includes  []config.Include
 }
 
-func GenerateAssignmentTemplate(bindings *TemplateBinding) (string, error) {
-	tmpl := template.Must(template.New("assignment").Funcs(sprig.TxtFuncMap()).Parse(sheetTemplate))
+func GenerateAssignmentTemplate(tpl *string, bindings *TemplateBinding) (string, error) {
+	if tpl == nil {
+		tpl = &defaultSheetTemplate
+	}
+	tmpl := template.Must(template.New("assignment").Funcs(sprig.TxtFuncMap()).Parse(*tpl))
 
 	var output bytes.Buffer
 
--- cli/internal/template/template_test.go
+++ cli/internal/template/template_test.go
@@ -3,10 +3,46 @@ package template
 import (
 	"testing"
 
+	"github.com/lithammer/dedent"
 	"github.com/zoomoid/assignments/v1/internal/config"
 )
 
-func TestGenerateAssignmentTemplate(t *testing.T) {
+func TestGenerateDefaultAssignmentTemplate(t *testing.T) {
+	bindings := TemplateBinding{
+		ClassPath: "../assignments",
+		Course:    "Example Course",
+		Group:     "Group Alpha",
+		Sheet:     "01",
+		Due:       "April 20th, 2021",
+		Includes: []config.Include{{
+			Path: "./math.tex",
+		}},
+		Members: []config.GroupMember{
+			{
+				Name: "Max Mustermann",
+				ID:   "123456",
+			},
+			{
+				Name: "Erika Mustermann",
+				ID:   "AB123456",
+			},
+		},
+	}
+
+	o, err := GenerateAssignmentTemplate(nil, &bindings)
+	t.Logf("Rendered template to %s", o)
+
+	if err != nil {
+		t.Fatalf(`GenerateAssignmentTemplate() should NOT return an error with this binding, %v`, err)
+	}
+
+	if len(o) == 0 {
+		t.Fatal(`GenerateAssignmentTemplate() should NOT return an empty string on this binding`)
+	}
+
+}
+
+func TestGenerateCustomAssignmentTemplate(t *testing.T) {
 	bindings := TemplateBinding{
 		ClassPath: "../assignments",
 		Course:    "Example Course",
@@ -25,7 +61,24 @@ func TestGenerateAssignmentTemplate(t *testing.T) {
 		},
 	}
 
-	o, err := GenerateAssignmentTemplate(&bindings)
+	template := dedent.Dedent(`
+		\documentclass{article}
+
+		\title{ {{- .Course -}} }
+		{{- $authors := list "" | compact -}}
+		{{- range $_, $m := .Members }}
+		{{- $authors = append $authors $m.Name -}}
+		{{- end }}
+		\author{ {{- $authors | join " \\and " -}} }
+		\date{ {{- .Due -}} }
+
+		\begin{document}
+		\maketitle
+
+		\end{document}
+	`)
+
+	o, err := GenerateAssignmentTemplate(&template, &bindings)
 	t.Logf("Rendered template to %s", o)
 
 	if err != nil {
@@ -35,5 +88,4 @@ func TestGenerateAssignmentTemplate(t *testing.T) {
 	if len(o) == 0 {
 		t.Fatal(`GenerateAssignmentTemplate() should NOT return an empty string on this binding`)
 	}
-
 }
