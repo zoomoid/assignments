1eef6582928328e173bd78bf4041134485fdfcb1
Alexander Bartolomey <occloxium@gmail.com>
2022-06-21 00:55:55 +0200

    chore(cli): add verbose flags
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/cmd/options/generic.go
+++ cli/cmd/options/generic.go
@@ -1,10 +1,12 @@
 package options
 
 const (
-	All        string = "all"
-	AllShort   string = "a"
-	Force      string = "force"
-	ForceShort string = "f"
-	Quiet      string = "quiet"
-	QuietShort string = "q"
+	All          string = "all"
+	AllShort     string = "a"
+	Force        string = "force"
+	ForceShort   string = "f"
+	Quiet        string = "quiet"
+	QuietShort   string = "q"
+	Verbose      string = "verbose"
+	VerboseShort string = "v"
 )
