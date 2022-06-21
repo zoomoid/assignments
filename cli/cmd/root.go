e3c2647b081768354371b64fabdb16414c7fe439
Alexander Bartolomey <occloxium@gmail.com>
2022-06-21 11:40:21 +0200

    refactor(cli): make root command less global-scoped
    moved a lot of functionality into functions, which makes the cmd directory
    way more testable than before
    
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,66 +1,91 @@
 package cmd
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/lithammer/dedent"
 	"github.com/spf13/cobra"
 	"github.com/zoomoid/assignments/v1/cmd/options"
 	"github.com/zoomoid/assignments/v1/internal/context"
 	"go.uber.org/zap"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "assignment",
-	Short: "assignments CLI for conveniently templating, building, and bundling course assignment",
-	Long: `💡 assignments CLI for conveniently templating, building, and bundling course assignment
-
-	Run 'assignment bootstrap' to create a new environment {needed}
-	Run 'assignment generate <number>' to template a new assignment
-	Run 'assignment build <number>' to build a specfic assignment with latexmk (add '--all' to build all assignments)
-	Run 'assignment compile <number>' to compile a zip file for a specific assignment (add '--all' to compile all assignments)
-	Run 'assignment release' inside a Gitlab CI/CD pipeline to create files required for automatic release (see manual)
-
-	See each command's help for description of arguments
-
-	Copyright (C) zoomoid, 2022`,
-
-	RunE: func(cmd *cobra.Command, args []string) error {
-		var l *zap.Logger
-		if verbose {
-			l, _ = zap.NewDevelopment()
-
-		} else {
-			l, _ = zap.NewProduction()
-		}
-		defer l.Sync()
-		sugar := l.Sugar()
-		logger = sugar
-
-		return fmt.Errorf("assignments requires a subcommand to run")
-	},
-}
-
-var logger *zap.SugaredLogger
-var cwd string
-var verbose bool
+var (
+	rootLongDescription = dedent.Dedent(`
+		Manangement CLI for doing university assignments with and around
+		the "csassignments" LaTeX class. 
+	`)
+)
 
 func Execute() {
 
-	rootCmd.Flags().BoolVarP(&verbose, options.Verbose, options.VerboseShort, false, "Prints debug logs")
+	var verbose bool
+	flag.BoolVar(&verbose, options.VerboseShort, false, "Prints debug logs")
+	flag.Parse()
 
-	dir, err := os.Getwd()
-
+	// program's working directory
+	pwd, err := os.Getwd()
 	if err != nil {
-		logger.Fatal("Failed to determine current working directory")
+		log.Fatal("Failed to determine current working directory")
 	}
 
-	cwd = dir
+	// if we cannot find a configuration file in here, traverse the file tree upwards
+	// until either the root or we find a config file
+	cfgPath, err := findConfigmapFile(pwd)
+	if err != nil {
+		log.Fatal("Failed to find configmap in working directory or above. Is the directory initialized?")
+	}
+
+	rootCmd := NewRootCommand(&rootOptions{
+		root:    cfgPath,
+		cwd:     pwd,
+		verbose: verbose,
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("%v", err)
+	}
+}
+
+type rootOptions struct {
+	root    string
+	cwd     string
+	verbose bool
+}
+
+type rootData struct {
+	*rootOptions
+	logger *zap.SugaredLogger
+}
+
+func NewRootCommand(opts *rootOptions) *cobra.Command {
+
+	logger, _ := makeLogger(opts.verbose)
+
+	data := &rootData{
+		rootOptions: opts,
+		logger:      logger,
+	}
+
+	rootCmd := &cobra.Command{
+		Use:   "assignment",
+		Short: "assignments CLI for conveniently templating, building, and bundling course assignment",
+		Long:  rootLongDescription,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("assignments requires a subcommand to run")
+		},
+	}
 
 	ctx := &context.AppContext{
-		Logger:        logger,
-		Cwd:           cwd,
+		Logger:        data.logger,
+		Cwd:           data.cwd,
+		Root:          data.root,
 		Configuration: nil,
 	}
 
@@ -69,7 +94,39 @@ func Execute() {
 	rootCmd.AddCommand(NewBuildCommand(ctx, nil))
 	rootCmd.AddCommand(NewBundleCommand(ctx, nil))
 
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatalf("%v", err)
+	return rootCmd
+}
+
+func findConfigmapFile(start string) (string, error) {
+	p := start
+	for {
+		_, err := os.Stat(p)
+		if err != nil {
+			if filepath.Base(filepath.ToSlash(p)) == "/" {
+				break
+			}
+			// move up one level and try again
+			p = filepath.Clean(fmt.Sprintf("%s/../", p))
+			continue
+		}
+		// found configmap file at p
+		return p, nil
 	}
+	return "", errors.New("failed to find configmap in working directory or above")
+}
+
+func makeLogger(verbose bool) (*zap.SugaredLogger, error) {
+	var l *zap.Logger
+	var err error
+	if verbose {
+		l, err = zap.NewDevelopment()
+
+	} else {
+		l, err = zap.NewProduction()
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer l.Sync()
+	return l.Sugar(), nil
 }
