87155a31703c96d43abefff1ce5fff3385945981
Alexander Bartolomey <occloxium@gmail.com>
2022-06-27 16:39:08 +0200

    docs(cli): add documentation for AppContext struct
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/internal/context/context.go
+++ cli/internal/context/context.go
@@ -5,10 +5,23 @@ import (
 	zap "go.uber.org/zap"
 )
 
+// AppContext is initialized by a cobra command and carried through the application
+// to provide a shared space of root-level configuration
 type AppContext struct {
-	Logger        *zap.SugaredLogger
-	Cwd           string
-	Root          string
+	// Logger is the shared application zap logger
+	Logger *zap.SugaredLogger
+	// Cwd is the working directory from which the command was invoked
+	Cwd string
+	// Root is the repository's root, either the Cwd, or any directory *above*
+	// Cwd that contains the .assignments.yaml file, marking it the repository's root
+	// This way, we can e.g. call `assignmentctl build -f .` in $ROOT/assignment-06
+	// to build assignment-06 without having to return to root
+	// This behaves similar to e.g. git when commands are used in subdirectories
+	Root string
+	// Contains the shared (read-in) Configuration struct used for e.g. build, generate,
+	// bundle and more. NOTE that is might not be present all the time, as it is only read
+	// from file when ctx.Read() is called. ctx.Read() should be followed by `defer ctx.Write()`
+	// in case any mutations to the struct happen to persist those back into the file
 	Configuration *config.Configuration
 }
 
