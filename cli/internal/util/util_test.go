5151339a6be27d4d08ded17e35db88abd33282dd
Alexander Bartolomey <occloxium@gmail.com>
2022-06-28 17:28:56 +0200

    test(cli): add unit tests for AssignmentNumberFromFilename
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/internal/util/util_test.go
+++ cli/internal/util/util_test.go
@@ -46,3 +46,86 @@ func TestAddLeadingZero(t *testing.T) {
 		}
 	})
 }
+
+func TestAssignmentNumberFromFilename(t *testing.T) {
+	t.Run("assignment-00", func(t *testing.T) {
+		a := "assignment-00"
+		expected := "00"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-1", func(t *testing.T) {
+		a := "assignment-1"
+		expected := "01"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-02", func(t *testing.T) {
+		a := "assignment-02"
+		expected := "02"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-10", func(t *testing.T) {
+		a := "assignment-10"
+		expected := "10"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-100", func(t *testing.T) {
+		a := "assignment-100"
+		expected := "100"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-010", func(t *testing.T) {
+		a := "assignment-010"
+		expected := "10"
+		r, err := AssignmentNumberFromFilename(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != expected {
+			t.Errorf("expected %s, found %s", expected, r)
+		}
+	})
+	t.Run("assignment-a010", func(t *testing.T) {
+		a := "assignment-a010"
+		r, err := AssignmentNumberFromFilename(a)
+		if err == nil {
+			t.Errorf("expected error, found %s", r)
+		}
+	})
+	t.Run("assignment--1", func(t *testing.T) {
+		a := "assignment--1"
+		r, err := AssignmentNumberFromFilename(a)
+		if err == nil {
+			t.Errorf("expected error, found %s", r)
+		}
+	})
+}
