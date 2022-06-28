bb68f95b1054772172abafdf42839f07960e495c
Alexander Bartolomey <occloxium@gmail.com>
2022-06-28 10:00:55 +0200

    refactor(cli): implement Find, Marshal, and Unmarshal in configmap
    * Rename ReadConfigmap/WriteConfigmap to Read/Write (similar to context)
    * Add Find function from root command
    * Add Marshal function for marshalling config to yaml
    * Add Unmarshal function for unmarshalling yaml to config
    
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cli/internal/config/configmap.go
+++ cli/internal/config/configmap.go
@@ -2,18 +2,31 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
-// ReadConfigMap reads in a config file an unmarshals it into a configuration struct
-func ReadConfigMap(path string) (*Configuration, error) {
+const (
+	ConfigurationFileName string = ".assignments.yaml"
+	ConfigurationFileType string = "yaml"
+)
+
+var (
+	ErrNoConfigmap error = errors.New("failed to find configmap in working directory or above")
+)
+
+// Read reads in a config file an unmarshals it into a configuration struct
+func Read(path string) (*Configuration, error) {
 	if path == "" {
 		path = "."
 	}
-	viper.SetConfigFile(".assignments.yaml")
-	viper.SetConfigType("yaml")
+	viper.SetConfigFile(ConfigurationFileName)
+	viper.SetConfigType(ConfigurationFileType)
 	viper.AddConfigPath(path)
 
 	if err := viper.ReadInConfig(); err != nil {
@@ -30,13 +43,13 @@ func ReadConfigMap(path string) (*Configuration, error) {
 	return config, nil
 }
 
-// WriteConfigMap converts a configuration struct into a map for viper to write to the filesystem as config file
-func WriteConfigMap(config *Configuration, path string) error {
+// Write converts a configuration struct into a map for viper to write to the filesystem as config file
+func Write(config *Configuration, path string) error {
 	if path == "" {
 		path = "."
 	}
-	viper.SetConfigFile(".assignments.yaml")
-	viper.SetConfigType("yaml")
+	viper.SetConfigFile(ConfigurationFileName)
+	viper.SetConfigType(ConfigurationFileType)
 	viper.AddConfigPath(path)
 
 	marshalledConfig, err := json.Marshal(config)
@@ -56,4 +69,40 @@ func WriteConfigMap(config *Configuration, path string) error {
 	}
 	err = viper.WriteConfig()
 	return err
+}
+
+// Find traverses the file system tree from the start path upwards in search of a
+// configmap file.
+//
+// Returns the directory where we found the file. If the root is reached without
+// finding a configmap, an error is returned
+func Find(start string) (string, error) {
+	p := start
+	for {
+		_, err := os.Stat(filepath.Join(p, ConfigurationFileName))
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				if p == filepath.Join(filepath.Dir(p), "..") {
+					// path does not change by traversal, already at root
+					break
+				}
+			}
+			// move up one level and try again
+			p = filepath.Join(p, "..")
+			continue
+		}
+		// found configmap file at p
+		return p, nil
+	}
+	return "", ErrNoConfigmap
+}
+
+// Unmarshal implements yaml unmarshalling for configuration structs
+func Unmarshal(in []byte, out *Configuration) error {
+	return yaml.Unmarshal(in, out)
+}
+
+// Marshal implements yaml marshalling for configuration structs
+func Marshal(in Configuration) (out []byte, err error) {
+	return yaml.Marshal(in)
 }
